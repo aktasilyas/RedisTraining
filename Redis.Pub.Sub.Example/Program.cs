0e39d8e7aca8770cc71ed074e1ea130a075fce21
ilyasaktass <ilyas.aktas@obrosoft.com>
2024-05-01 16:35:24 +0300

    Redisin pub/sub özelliği kullanıldı

---
+++ Redis.Pub.Sub.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+
+ISubscriber subscriber =  connectionMultiplexer.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Message:");
+    var message = Console.ReadLine();
+    await subscriber.PublishAsync("myChannel", message);
+}
